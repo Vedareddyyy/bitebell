51d1733c47d50b9418271ca72d07c285c84f7c30
Vedareddy <srivedareddypanyala@gmail.com>
2025-08-21 16:50:00 +0530

    jgshj

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BiteBell.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
